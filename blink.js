dd7293d9f2f0f855a0f831662f786c7eb844a92f
Rasmus "Pez" Wejlgaard <pez@pez.sh>
2023-02-24 22:46:23 +0000

    Create blink.js

---
+++ blink.js
@@ -0,0 +1,24 @@
+t.prefs_.set(
+  'color-palette-overrides',
+  [
+    "#ffffff", 
+    "#ff0000", 
+    "#00ff00", 
+    "#ffff00", 
+    "#0000ff", 
+    "#0000ff", 
+    "#00ffff", 
+    "#ffffff", 
+    "#ffffff", 
+    "#ff0000", 
+    "#00ff00", 
+    "#ffff00", 
+    "#1081d6", 
+    "#a000ff", 
+    "#0f7ddb", 
+    "#ffffff"
+  ]
+);
+t.prefs_.set('foreground-color', "#ffffff");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(185,185,185,0.5)');
